963a67f7663459061d4e3712717c1bd6f75218c2
Tam Long <aqua.tamlong@gmail.com>
2023-10-02 07:15:20 +0700

    Ex1-7 react components

---
+++ exercises/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+export default function Navigation() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#home" className="active">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
